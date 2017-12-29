eb31fbafe9fb523d9b1808241b8e7feef4520c10
michal2409 <michal.futrega@gmail.com>
2017-12-29 15:22:26 +0100

    Add files via upload

---
+++ Projekt_2/IFSpremium/R/KochCurve-data.R
@@ -0,0 +1,12 @@
+#' Koch curve fractal.
+#'
+#' List of contractions for Koch curve.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name KochCurve
+#' @format An object of class \code{'list'} of length 4.
+#' @examples
+#' plot(createIFS(KochCurve), 7)
+
+"KochCurve"
