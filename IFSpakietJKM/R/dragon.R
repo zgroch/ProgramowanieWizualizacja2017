340661c89fa950d767135ee99810e9d9ab69f305
majaszl <33073529+majaszl@users.noreply.github.com>
2018-01-02 01:46:59 +0100

    Add files via upload

---
+++ IFSpakietJKM/R/dragon.R
@@ -0,0 +1,12 @@
+#' An example how to create plot of Heighway dragon.
+#'
+#' The dataset contains functions which allow to create a IFS object. The object is used to plot the fractal named Heigway dragon.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name dragon
+#'
+#'@examples
+#'createIFS(dragon)
+#'
+"dragon"
